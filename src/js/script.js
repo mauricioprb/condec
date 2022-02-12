eca90854bffb67094b89095743a14f6ebabeb831
bernardoviero <bernardodesouviero@hotmail.com>
2022-02-12 02:03:21 -0300

    script

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,7 @@
 function binarioParaDecimal(binario) {
+    let binario = document.getElementById("binario").value;
     let decimal = 0;
     for (let i = 0; i < binario.length; i++)
         decimal += Math.pow(2, i) * binario[binario.length - i - 1]
     return `${decimal}`
 }
-console.log(binarioParaDecimal('11111100100'))
