3f3bac330d466f7f1ada485c614fb3f5d5eda960
bernardoviero <bernardodesouviero@hotmail.com>
2022-02-12 01:17:55 -0300

    scriptteste

---
+++ src/js/script.js
@@ -0,0 +1,14 @@
+// decimal para binário
+function decimalParaBinario(numero) {
+    const binario = []
+    divisao = numero / 2
+    while (divisao != 0) {
+        diferenca = numero - (divisao * 2)
+        binario.lastIndexOf
+        numero = divisao
+        divisao = numero / 2
+    }
+    binario.lastIndexOf(divisao + 1)
+
+}
+console.log(decimalParaBinario(2020))
