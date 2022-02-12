49fe8b7a03d963295c5cff5d3ac7635c1020d77a
bernardoviero <bernardodesouviero@hotmail.com>
2022-02-12 02:17:51 -0300

    script

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,10 @@
-let binario = document.getElementById("binario");
-
-function binarioParaDecimal(binario) {
-    let resultado = 0;
-    for (let i = 0; i < binario.length; i++)
-        resultado += Math.pow(2, i) * binario[binario.length - i - 1]
-    return `${resultado}`
+document.getElementById("Converter").onkeypress = function(e) {
+    if (e.keyCode == "Converter") {
+        function binarioParaDecimal(binario) {
+            let resultado = 0;
+            for (let i = 0; i < binario.length; i++)
+                resultado += Math.pow(2, i) * binario[binario.length - i - 1]
+            return `${resultado}`
+        }
+    }
 }
