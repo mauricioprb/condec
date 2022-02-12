ace48d1abf696294c1767573335f5ab2a1d9db9b
bernardoviero <bernardodesouviero@hotmail.com>
2022-02-12 14:59:49 -0300

    script

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,15 @@
-let text = document.querySelector("binario");
-var binario = input.binario;
+let btn = document.querySelector("resultado");
+btn.addEventListener("click", function(e) {
+    e.preventDefault();
+
+    const resultado = document.querySelector("converterDec");
+
+    const binario = resultado.binario
+})
 
 function binarioParaDecimal(binario) {
     let resultado = 0;
     for (let i = 0; i < binario.length; i++)
         resultado += Math.pow(2, i) * binario[binario.length - i - 1]
-    console.log()
+    console.log(`${binario}`)
 }
