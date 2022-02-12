19e336a6169583b776a7464f5ea613e27ff7dd03
bernardoviero <bernardodesouviero@hotmail.com>
2022-02-12 02:14:49 -0300

    script

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,6 @@
-function binarioParaDecimal(binario) {
-    let binario = document.getElementById("binario").value;
+let binario = document.getElementById("binario");
 
+function binarioParaDecimal(binario) {
     let resultado = 0;
     for (let i = 0; i < binario.length; i++)
         resultado += Math.pow(2, i) * binario[binario.length - i - 1]
