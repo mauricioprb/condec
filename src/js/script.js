5f46cb119f296441a120e576337c9b2a63bce047
Mauricio P. Braga <61766272+mauriciopbraga@users.noreply.github.com>
2022-02-12 21:59:25 -0300

    a

--- src/js/script.js
+++
@@ -1,9 +0,0 @@
-function converterDecimal(numero) {
-    let decimal = parseInt(numero)
-    let binario = (num % 2).toString()
-    for (; num > 1;) {
-        num = parseInt(num / 2);
-        binario = (num % 2) + (binario);
-    }
-    document.getElementById("resultado").innerText = numero;
-}
