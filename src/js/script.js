1535fb4dd884d6287ef5cd93040bb56680dc93a4
bernardoviero <bernardodesouviero@hotmail.com>
2022-02-14 11:29:15 -0300

    scirpt

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,5 @@
-let input = document.getElementById("decimal");
-input.addEventListener("keyup", function(event) {
-    if (event.keyCode === 13) {
-        event.preventDefault();
-        document.getElementById("myBtn").click();
-
-        function converterDec(numero) {
-            let decimal = parseInt(numero)
-            let binario = decimal.toString(2)
-            document.getElementById("resultado").innerText = binario;
-        }
-    }
-});
+function converterDec(numero) {
+    let decimal = parseInt(numero)
+    let binario = decimal.toString(2)
+    document.getElementById("resultado").innerText = binario;
+}
