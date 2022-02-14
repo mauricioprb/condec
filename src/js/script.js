ad494e1cfc2b05ad13de588242bf53672c2ddcd6
bernardoviero <bernardodesouviero@hotmail.com>
2022-02-14 11:16:45 -0300

    script

--- src/js/script.js
+++ src/js/script.js
@@ -2,4 +2,12 @@ function converterDec(numero) {
     let decimal = parseInt(numero)
     let binario = decimal.toString(2)
     document.getElementById("resultado").innerText = binario;
+
+    let input = document.getElementById("decimal");
+    input.addEventListener("keyup", function(event) {
+        if (event.keyCode === 13) {
+            event.preventDefault();
+            document.getElementById("myBtn").click();
+        }
+    });
 }
