1bc9dc4f889f8ea51c2714f00059f0b20feccdaf
bernardoviero <bernardodesouviero@hotmail.com>
2022-02-14 11:46:55 -0300

    scairpt

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,5 @@
 function converterDec(numero) {
-    let decimal = parseInt(numero)
-    let decimal = (Number(document.getElementById('decimal').value);
-        let binario = decimal.toString(2)
-        document.getElementById('decimal').value. - = tot
-    }
+    let decimal = parseInt(document.getElementById('decimal').value)
+    let binario = decimal.toString(2)
+    document.getElementById('decimal').innerHTML = binario;
+}
