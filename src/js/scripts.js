da2e2bb29adfce72234a88f922812e6f8e6cb0f0
Vinícius Guasso <viniguasso10@gmail.com>
2022-03-29 20:04:46 -0300

    numero aparacendo instantaneo

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,5 +1,8 @@
-function converterDec(numero) {
+function temperatureConverter(valNum) {
+    document.getElementById("outputBinario").innerHTML=valNum;
+  }
+/*function converterDec(numero) {
     let decimal = parseInt(document.getElementById('decimal').value)
     let binario = decimal.toString(2)
     document.getElementById('binario').innerHTML = binario;
-}
+}*/
