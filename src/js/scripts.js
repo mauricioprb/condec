ff17ced4ce82ce62c5205e7c067fa1664359d98e
Vinícius Guasso <viniguasso10@gmail.com>
2022-03-29 20:07:32 -0300

    .

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,6 +1,6 @@
 function temperatureConverter(valNum) {
     document.getElementById("outputBinario").innerHTML=valNum;
-  }
+}
 /*function converterDec(numero) {
     let decimal = parseInt(document.getElementById('decimal').value)
     let binario = decimal.toString(2)
