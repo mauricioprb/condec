678710ac1adcf048017a59fd924dd893998a93f5
mauricio <mauriciopereirabraga@gmail.com>
2022-03-31 15:47:31 -0300

    main

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,21 @@
-function decimalConverter(conv,numero) {
-    numero = parseFloat(numero);
-    let inputDecimal = document.getElementById("inputDecimal");
-    let inputBinario = document.getElementById("inputBinario");
-    if (conv=="inputDecimal") {
-        let binario = numero.toString(2);
-        inputBinario.value=(binario);
+function decimalConverter(conv, numero){
+    numero = parseFloat(numero)
+
+    let inputDecimal = document.getElementById("inputDecimal")
+    let inputBinario = document.getElementById("inputBinario")
+
+    if(conv == "inputDecimal"){
+        let binario = numero.toString(2)
+        inputBinario.value = (binario)
     }
     if(conv == "inputBinario"){
-    	lastDigit = numero % 10;   
-        if (lastDigit == 1 || lastDigit == 0) {
+    	lastDigit = numero % 10
+        
+        if(lastDigit == 1 || lastDigit == 0){
         	let decimal = parseInt(numero, 2)
         	inputDecimal.value = (decimal)
         }
-        else {
+        else{
             inputDecimal.value = NaN
         }  
     }
