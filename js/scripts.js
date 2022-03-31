84b39fb8796a2c35363396a788cc8e755ab7b8af
mauricio <mauriciopereirabraga@gmail.com>
2022-03-31 01:39:22 -0300

    organizado

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,15 @@
-function decimalConverter(conv,numero) {
-    numero = parseFloat(numero);
-    let inputDecimal = document.getElementById("inputDecimal");
-    let inputBinario = document.getElementById("inputBinario");
-    if (conv=="inputDecimal") {
-        let binario = numero.toString(2);
-        inputBinario.value=(binario);
+function decimalConverter(conv, numero){
+    numero = parseFloat(numero)
+
+    let inputDecimal = document.getElementById("inputDecimal")
+    let inputBinario = document.getElementById("inputBinario")
+
+    if(conv == "inputDecimal"){
+        let binario = numero.toString(2)
+        inputBinario.value = (binario)
     }
-    if (conv=="inputBinario") {
-        let decimal = parseInt(numero, 2);
-        inputDecimal.value=(decimal);
+    if(conv == "inputBinario"){
+        let decimal = parseInt(numero, 2)
+        inputDecimal.value = (decimal)
     }
 }
